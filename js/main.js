221ff9b3f4c8f3a3f943871ec8529ec5f5275b23
Ira Shoemo <irashoemo1@gmail.com>
2023-03-06 20:12:47 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const visitorCounter = document.querySelector('#visitor-counter');
+// document.querySelector('#visitor-button').addEventListener('click', visitorCount)
+
+
+fetch('https://n7p1hha2i6.execute-api.us-east-2.amazonaws.com/dev/visitors/1')
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        console.log(data)
+        visitorCounter.innerText = data.count
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
